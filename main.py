cd84810a3aade26e9ac06e1bcade2c3d256b0b68
PaulWentzel1 <paul19wen@gmail.com>
2025-03-02 22:25:07 +0100

    Added function for european puts

--- main.py
+++ main.py
@@ -1,27 +1,51 @@
 import math
 from statistics import NormalDist
 
-def bs_call(s: float, k: float, r: float, o: float, t: float) -> float:
+
+def bs_call_stdlib(s: float, k: float, r: float, sigma: float, t: float) -> float:
     """
+    Calculates the price of a european call option using the Python standard library
 
     Args:
         s (_float_): Current price of the underlying
         k (_float_): Strike price
         r (_float_): Risk-free rate (Annual)
-        o (_float_): Volatility of the underlying (Annual)
+        sigma (_float_): Volatility of the underlying (Annual)
         t (_float_): Time to expiration
 
     Returns:
         float: Price of the call option
     """
 
-    d1 = ( math.log( s/k ) + ( r + ( o**2 / 2 ) ) * t )  /  (o * math.sqrt(t))
-    d2 = d1 - o*math.sqrt(t)
+    d1 = ( math.log( s/k ) + ( r + ( sigma ** 2 / 2 ) ) * t )  /  (sigma * math.sqrt(t))
+    d2 = d1 - sigma * math.sqrt(t)
+    
     call_price = NormalDist().cdf(d1) * s - NormalDist().cdf(d2) * k * ( math.exp(-r*t) ) 
     
     return call_price
 
 
+def bs_put_stdlib(s: float, k: float, r: float, sigma: float, t: float) -> float:
+    """
+    Calculates the price of a european put option using the Python standard library
+
+    Args:
+        s (_float_): Current price of the underlying
+        k (_float_): Strike price
+        r (_float_): Risk-free rate (Annual)
+        sigma (_float_): Volatility of the underlying (Annual)
+        t (_float_): Time to expiration
+
+    Returns:
+        float: Price of the put option
+    """
+
+    d1 = ( math.log( s/k ) + ( r + ( sigma ** 2 / 2 ) ) * t )  /  (sigma * math.sqrt(t))
+    d2 = d1 - sigma * math.sqrt(t)
+    
+    put_price = NormalDist().cdf(-d2) * k * (math.exp(-r*t)) - NormalDist().cdf(-d1) * s
+    
+    return put_price
 
 
 if __name__ == "__main__":
@@ -31,5 +55,5 @@ if __name__ == "__main__":
     risk_free_rate = 0.05
     volatility = 0.20
 
-    print(f"Call Option price: {bs_call(stock_price, strike_price, risk_free_rate, volatility, time_to_maturity):.4f}")
-
+    print(f"Call Option price: {bs_call_stdlib(stock_price, strike_price, risk_free_rate, volatility, time_to_maturity):.4f}")
+    print(f"Put Option price: {bs_put_stdlib(stock_price, strike_price, risk_free_rate, volatility, time_to_maturity):.4f}")
