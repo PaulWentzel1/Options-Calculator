e49f89cd65462e6e010362a1a57d0c26eeaba191
PaulWentzel1 <paul19wen@gmail.com>
2025-03-23 22:54:56 +0100

    Update greeks_bsm.py

--- greeks_bsm.py
+++ greeks_bsm.py
@@ -212,7 +212,7 @@ def vega_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.ar
 
 def theta_call_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, d1: float = None, d2: float = None, trading_days: int = None):
     """
-    Calculates the theta of a european option call, whose underlying pays a continuous dividend yield q, using the Python standard library
+    Calculates the theta of a european call option, whose underlying pays a continuous dividend yield q, using the Python standard library
     Returns the yearly Theta, divide by number of days to get daily theta
 
     Args:
@@ -227,7 +227,7 @@ def theta_call_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: f
         trading_days (_int_): The number of trading days in a year
         
     Returns:
-        theta: The theta of the option
+        float: The theta of the option
     """
     if t <= 0:
         raise ValueError("Time to expiration (t) must be positive.")
@@ -248,7 +248,7 @@ def theta_call_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: f
 
 def theta_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, d1: np.array = None, d2: np.array = None, trading_days: int = None):
     """
-    Calculates the theta of a european option call, whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
+    Calculates the theta of a european call option, whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
     Returns the yearly Theta, divide by number of days to get daily theta
 
     Args:
@@ -263,7 +263,7 @@ def theta_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t:
         trading_days (_int_): The number of trading days in a year
         
     Returns:
-        theta: The theta of the option
+        np.array: The theta of the option
     """
     if t <= 0:
         raise ValueError("Time to expiration (t) must be positive.")
@@ -284,7 +284,7 @@ def theta_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t:
 
 def theta_put_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, d1: float = None, d2: float = None, trading_days: int = None):
     """
-    Calculates the theta of a european option put, whose underlying pays a continuous dividend yield q, using the Python standard library
+    Calculates the theta of a european put option, whose underlying pays a continuous dividend yield q, using the Python standard library
     Returns the yearly Theta, divide by number of days to get daily theta
 
     Args:
@@ -299,7 +299,7 @@ def theta_put_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: fl
         trading_days (_int_): The number of trading days in a year
         
     Returns:
-        theta: The theta of the option
+        float: The theta of the option
     """
     if t <= 0:
         raise ValueError("Time to expiration (t) must be positive.")
@@ -320,7 +320,7 @@ def theta_put_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: fl
 
 def theta_put_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, d1: np.array = None, d2: np.array = None, trading_days: int = None):
     """
-    Calculates the theta of a european option put, whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
+    Calculates the theta of a european put option, whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
     Returns the yearly Theta, divide by number of days to get daily theta
 
     Args:
@@ -335,7 +335,7 @@ def theta_put_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: 
         trading_days (_int_): The number of trading days in a year
         
     Returns:
-        theta: The theta of the option
+        np.array: The theta of the option
     """
     if t <= 0:
         raise ValueError("Time to expiration (t) must be positive.")
@@ -412,7 +412,7 @@ def theta_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.a
 
 def rho_call_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, d1: float = None, d2: float = None):
     """
-    Calculates the rho of a european option call, whose underlying pays a continuous dividend yield q, using the Python standard library
+    Calculates the rho of a european call option, whose underlying pays a continuous dividend yield q, using the Python standard library
     Divide by 100 to get the rho as option price change for 1%
 
     Args:
@@ -426,7 +426,7 @@ def rho_call_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: flo
         d2 (_float_): The precalculated value for d2
         
     Returns:
-        theta: The theta of the option
+        float: The theta of the option
     """
 
     if d1 == None:
@@ -442,7 +442,7 @@ def rho_call_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: flo
 
 def rho_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, d1: np.array = None, d2: np.array = None):
     """
-    Calculates the rho of a european option call, whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
+    Calculates the rho of a european call option, whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
     Divide by 100 to get the rho as option price change for 1%
 
     Args:
@@ -456,7 +456,7 @@ def rho_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: n
         d2 (_np.array_): The precalculated value for d2
         
     Returns:
-        theta: The theta of the option
+        np.array: The rho of the option
     """
     if t <= 0:
         raise ValueError("Time to expiration (t) must be positive.")
@@ -474,7 +474,7 @@ def rho_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: n
 
 def rho_put_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, d1: float = None, d2: float = None):
     """
-    Calculates the rho of a european option put, whose underlying pays a continuous dividend yield q, using the Python standard library
+    Calculates the rho of a european put option, whose underlying pays a continuous dividend yield q, using the Python standard library
     Divide by 100 to get the rho as option price change for 1%
 
     Args:
@@ -488,7 +488,7 @@ def rho_put_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: floa
         d2 (_float_): The precalculated value for d2
         
     Returns:
-        theta: The theta of the option
+        float: The rho of the option
     """
 
     if d1 == None:
@@ -504,7 +504,7 @@ def rho_put_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: floa
 
 def rho_put_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, d1: np.array = None, d2: np.array = None):
     """
-    Calculates the rho of a european option put, whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
+    Calculates the rho of a european put option, whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
     Divide by 100 to get the rho as option price change for 1%
 
     Args:
@@ -518,7 +518,7 @@ def rho_put_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np
         d2 (_np.array_): The precalculated value for d2
         
     Returns:
-        theta: The theta of the option
+        np.array: The rho of the option
     """
     if t <= 0:
         raise ValueError("Time to expiration (t) must be positive.")
@@ -587,13 +587,297 @@ def rho_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.arr
     else:
         raise ValueError("Invalid option type")
 
+def epsilon_call_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, d1: float = None):
+    """
+    Calculates the epsilon of a european call option, whose underlying pays a continuous dividend yield q, using the Python standard library
 
-print(rho_call_stdlib(220,220,0,0.25,1,0))
-print(rho_call_vector(220,220,0,0.25,1,0))
-print(rho_stdlib(220,220,0,0.25,1,0,flag="c"))
-print(rho_vector(220,220,0,0.25,1,0,flag="c"))
+    Args:
+        s (_float_): Current price of the underlying
+        k (_float_): Strike price
+        r (_float_): Risk-free rate (Annual)
+        sigma (_float_): Volatility of the underlying (Annual)
+        t (_float_): Time to expiration
+        q (_float_): Continuous dividend yield
+        d1 (_float_): The precalculated value for d1
+        
+    Returns:
+        float: The epsilon of the option
+    """
 
-print(rho_put_stdlib(220,220,0,0.25,1,0))
-print(rho_put_vector(220,220,0,0.25,1,0))
-print(rho_stdlib(220,220,0,0.25,1,0,flag="p"))
-print(rho_vector(220,220,0,0.25,1,0,flag="p"))
+    if d1 == None:
+        d1 = ( math.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * math.sqrt(t))
+    
+
+    rho = -s * t * math.exp(-q*t) * NormalDist().cdf(d1)
+
+    return rho
+
+
+def epsilon_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, d1: np.array = None):
+    """
+    Calculates the epsilon of a european call option, whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
+
+    Args:
+        s (_np.array_): Current price of the underlying
+        k (_np.array_): Strike price
+        r (_np.array_): Risk-free rate (Annual)
+        sigma (_np.array_): Volatility of the underlying (Annual)
+        t (_np.array_): Time to expiration
+        q (_np.array_): Continuous dividend yield
+        d1 (_np.array_): The precalculated value for d1
+        
+    Returns:
+        np.array: The epsilon of the option
+    """
+
+    if d1 == None:
+        d1 = ( np.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * np.sqrt(t))
+    
+    rho = -s * t * np.exp(-q*t) * norm.cdf(d1)
+
+    return rho
+
+
+def epsilon_put_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, d1: float = None):
+    """
+    Calculates the epsilon of a european put option, whose underlying pays a continuous dividend yield q, using the Python standard library
+
+    Args:
+        s (_float_): Current price of the underlying
+        k (_float_): Strike price
+        r (_float_): Risk-free rate (Annual)
+        sigma (_float_): Volatility of the underlying (Annual)
+        t (_float_): Time to expiration
+        q (_float_): Continuous dividend yield
+        d1 (_float_): The precalculated value for d1
+        
+    Returns:
+        float: The epsilon of the option
+    """
+
+    if d1 == None:
+        d1 = ( math.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * math.sqrt(t))
+    
+    rho = s * t * math.exp(-q*t) * NormalDist().cdf(-d1)
+
+    return rho
+
+
+def epsilon_put_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, d1: np.array = None):
+    """
+    Calculates the epsilon of a european put option, whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
+
+    Args:
+        s (_np.array_): Current price of the underlying
+        k (_np.array_): Strike price
+        r (_np.array_): Risk-free rate (Annual)
+        sigma (_np.array_): Volatility of the underlying (Annual)
+        t (_np.array_): Time to expiration
+        q (_np.array_): Continuous dividend yield
+        d1 (_np.array_): The precalculated value for d1
+        
+    Returns:
+        np.array: The epsilon of the option
+    """
+
+    if d1 == None:
+        d1 = ( np.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * np.sqrt(t))
+    
+    rho = s * t * np.exp(-q*t) * norm.cdf(-d1)
+
+    return rho
+
+
+def epsilon_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, flag: str, d1: float = None) -> float:
+    """
+    Calculates the epsilon of a european option (call or put), whose underlying pays a continuous dividend yield q, using the Python standard library
+
+    Args:
+        s (_float_): Current price of the underlying
+        k (_float_): Strike price
+        r (_float_): Risk-free rate (Annual)
+        sigma (_float_): Volatility of the underlying (Annual)
+        t (_float_): Time to expiration
+        q (_float_): Continuous dividend yield
+        flag (_str_): Determines the type of the option
+        d1 (_float_): The precalculated value for d1
+
+    Returns:
+        float: The epsilon of the option
+    """
+
+    if flag.lower() == "c":
+        return epsilon_call_stdlib(s, k, r, sigma, t, q, d1, d2)
+    elif flag.lower() == "p":
+        return epsilon_put_stdlib(s, k, r, sigma, t, q, d1, d2)
+    else:
+        raise ValueError("Invalid option type")
+
+
+def epsilon_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, flag: str, d1: np.array = None) -> np.array:
+    """
+    Calculates the epsilon of a european option (call or put), whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
+
+    Args:
+        s (_np.array_): Current price of the underlying
+        k (_np.array_): Strike price
+        r (_np.array_): Risk-free rate (Annual)
+        sigma (_np.array_): Volatility of the underlying (Annual)
+        t (_np.array_): Time to expiration
+        q (_np.array_): Continuous dividend yield
+        flag (_str_): Determines the type of the option
+        d1 (_np.array_): The precalculated value for d1
+
+    Returns:
+        np.array: The epsilon of the option
+    """
+
+    if flag.lower() == "c":
+        return	epsilon_call_vector(s, k, r, sigma, t, q, d1, d2)
+    elif flag.lower() == "p":
+        return	epsilon_put_vector(s, k, r, sigma, t, q, d1, d2)
+    else:
+        raise ValueError("Invalid option type")
+
+def gamma_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, d1: float = None):
+    """
+    Calculates the gamma of a european (Call or Put) option, whose underlying pays a continuous dividend yield q, using the Python standard library
+
+    Args:
+        s (_float_): Current price of the underlying
+        k (_float_): Strike price
+        r (_float_): Risk-free rate (Annual)
+        sigma (_float_): Volatility of the underlying (Annual)
+        t (_float_): Time to expiration
+        q (_float_): Continuous dividend yield
+        d1 (_float_): The precalculated value for d1
+        
+    Returns:
+        float: The gamma of the option
+    """
+
+    if d1 == None:
+        d1 = ( math.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * math.sqrt(t))
+
+    gamma = math.exp(-q*t) * ( NormalDist().pdf(d1) / (s * sigma * math.sqrt(t)) )
+
+    return gamma
+
+
+def gamma_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, flag: str, d1: np.array = None) -> np.array:
+    """
+    Calculates the gamma of a european option (call or put), whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
+
+    Args:
+        s (_np.array_): Current price of the underlying
+        k (_np.array_): Strike price
+        r (_np.array_): Risk-free rate (Annual)
+        sigma (_np.array_): Volatility of the underlying (Annual)
+        t (_np.array_): Time to expiration
+        q (_np.array_): Continuous dividend yield
+        flag (_str_): Determines the type of the option
+        d1 (_np.array_): The precalculated value for d1
+
+    Returns:
+        np.array: The gamma of the option
+    """
+
+    if d1 == None:
+        d1 = ( np.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * np.sqrt(t))
+
+    gamma = np.exp(-q*t) * ( norm.pdf(d1) / (s * sigma * np.sqrt(t)) )
+
+    return gamma
+
+
+def vanna_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, d1: float = None, d2: float = None):
+    """
+    Calculates the vanna of a european (Call or Put) option, whose underlying pays a continuous dividend yield q, using the Python standard library
+
+    Args:
+        s (_float_): Current price of the underlying
+        k (_float_): Strike price
+        r (_float_): Risk-free rate (Annual)
+        sigma (_float_): Volatility of the underlying (Annual)
+        t (_float_): Time to expiration
+        q (_float_): Continuous dividend yield
+        d1 (_float_): The precalculated value for d1
+        d2 (_float_): The precalculated value for d2
+        
+    Returns:
+        float: The vanna of the option
+    """
+
+    if d1 == None:
+        d1 = ( math.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * math.sqrt(t))
+
+    if d2 == None:
+        d2 = d1 - sigma * np.sqrt(t)
+
+    vanna = -math.exp(-q*t) * NormalDist().pdf(d1) * (d2 / sigma)
+
+    return vanna
+
+
+def vanna_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, flag: str, d1: np.array = None d2: np.array = None) -> np.array:
+    """
+    Calculates the vanna of a european option (call or put), whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
+
+    Args:
+        s (_np.array_): Current price of the underlying
+        k (_np.array_): Strike price
+        r (_np.array_): Risk-free rate (Annual)
+        sigma (_np.array_): Volatility of the underlying (Annual)
+        t (_np.array_): Time to expiration
+        q (_np.array_): Continuous dividend yield
+        flag (_str_): Determines the type of the option
+        d1 (_np.array_): The precalculated value for d1
+        d2 (_np.array_): The precalculated value for d2
+
+    Returns:
+        np.array: The vanna of the option
+    """
+
+    if d1 == None:
+        d1 = ( np.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * np.sqrt(t))
+
+    if d2 == None:
+        d2 = d1 - sigma * np.sqrt(t)
+
+    vanna = -np.exp(-q*t) * norm.pdf(d1) * (d2 / sigma)
+    
+    return vanna
+
+
+def charm_call_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, d1: float = None, d2: float = None):
+    """
+    Calculates the charm of a european call option, whose underlying pays a continuous dividend yield q, using the Python standard library
+
+    Args:
+        s (_float_): Current price of the underlying
+        k (_float_): Strike price
+        r (_float_): Risk-free rate (Annual)
+        sigma (_float_): Volatility of the underlying (Annual)
+        t (_float_): Time to expiration
+        q (_float_): Continuous dividend yield
+        d1 (_float_): The precalculated value for d1
+        d2 (_float_): The precalculated value for d2
+        
+    Returns:
+        float: The charm of the option
+    """
+
+    if d1 == None:
+        d1 = ( math.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * math.sqrt(t))
+
+    if d2 == None:
+        d2 = d1 - sigma * np.sqrt(t)
+
+    charm = q * math.exp(-q*t) * NormalDist().cdf(d1) - math.exp(-q*t) * NormalDist().cdf(d1) * ( ( 2*(r-q)* t - d2*sigma * math.sqrt(t) ) / (2 * t * sigma * math.sqrt(t)) )
+
+    return charm
+
+
+def charm_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, d1: np.array = None, d2: np.array = None):
+def charm_put_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, d1: float = None, d2: float = None):
+def charm_put_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, d1: np.array = None, d2: np.array = None):
