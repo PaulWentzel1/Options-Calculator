b818e94ac4ca51834946e872f0fbe65fc8986553
Paul Wentzel <paul19wen@gmail.com>
2025-03-25 11:36:02 +0100

    Update pricing.py

--- pricing.py
+++ pricing.py
@@ -1,8 +1,57 @@
 def black_scholes_merton():
     pass
 
-def black_scholes():
-    pass
+def black_scholes_stdlib(s: float, k: float, r: float, sigma: float, t: float, flag: str, d1: float = None, d2: float = None) -> float:
+    """
+    Calculates the price of a european option (call or put), whose underlying pays no dividends, using the Python standard library
+
+    Args:
+        s (_float_): Current price of the underlying
+        k (_float_): Strike price
+        r (_float_): Risk-free rate (Annual)
+        sigma (_float_): Volatility of the underlying (Annual)
+        t (_float_): Time to expiration
+        flag (_str_): Determines the type of the option
+        d1 (_float_): The precalculated value for d1
+        d2 (_float_): The precalculated value for d2
+
+    Returns:
+        float: The price of the option
+    """
+    
+    if flag.lower() == "c":
+        return bs_call_stdlib(s, k, r, sigma, t, d1, d2)
+    elif flag.lower() == "p":
+        return bs_put_stdlib(s, k, r, sigma, t, d1, d2)
+    else:
+        raise ValueError("Invalid option type")   
+	
+
+
+def black_scholes_stdlib(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.arra, flag: str, d1: np.array = None, d2: np.array = None) -> np.array:
+    """
+    Calculates the delta of a european option (call or put), whose underlying pays no dividends, using numpy & scipy for speed & vectorized operations
+
+    Args:
+        s (_np.array_): Current price of the underlying
+        k (_np.array_): Strike price
+        r (_np.array_): Risk-free rate (Annual)
+        sigma (_np.array_): Volatility of the underlying (Annual)
+        t (_np.array_): Time to expiration
+        flag (_str_): Determines the type of the option
+        d1 (_np.array_): The precalculated value for d1
+        d2 (_float_): The precalculated value for d2
+        
+    Returns:
+        np.array: The price of the option
+    """
+
+    if flag.lower() == "c":
+        return	bs_call_vector(s, k, r, sigma, t, q, d1, d2)
+    elif flag.lower() == "p":
+        return	bs_put_vector(s, k, r, sigma, t, q, d1, d2)
+    else:
+        raise ValueError("Invalid option type")
 
 
 
