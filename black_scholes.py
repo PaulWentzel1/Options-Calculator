7a985d4e405bb536116eb5c95f746ef4e88d43b9
PaulWentzel1 <paul19wen@gmail.com>
2025-03-30 21:45:52 +0200

    Added new greeks, reworked file structure

--- black_scholes.py
+++ black_scholes.py
@@ -1,5 +1,50 @@
-def black_scholes_merton():
-    pass
+import math
+from statistics import NormalDist
+
+import numpy as np
+from scipy.stats import norm
+
+def d1_d2_stdlib(s: float, k: float, r: float, sigma: float, t: float) -> tuple[float, float]:
+    """
+    Calculates d1 and d2 for a european option (call or put), whose underlying pays no dividends, using the Python standard library
+
+    Args:
+        s (_float_): Current price of the underlying
+        k (_float_): Strike price
+        r (_float_): Risk-free rate (Annual)
+        sigma (_float_): Volatility of the underlying (Annual)
+        t (_float_): Time to expiration
+
+    Returns:
+        tuple[float, float]: d1 and d2
+    """
+
+    d1 = ( math.log( s/k ) + ( r + ( sigma ** 2 / 2 ) ) * t )  /  (sigma * math.sqrt(t))
+    d2 = d1 - sigma * math.sqrt(t)
+
+    return d1, d2
+
+
+def d1_d2_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array) -> tuple[np.array, np.array]:
+    """
+    Calculates d1 and d2 for a european option (call or put), whose underlying pays no dividends, for speed & vectorized operations
+
+    Args:
+        s (_np.array_): Current price of the underlying
+        k (_np.array_): Strike price
+        r (_np.array_): Risk-free rate (Annual)
+        sigma (_np.array_): Volatility of the underlying (Annual)
+        t (_np.array_): Time to expiration
+
+    Returns:
+        tuple[np.array, np.array]: d1 and d2
+    """
+
+    d1 = ( np.log( s/k ) + ( r + ( sigma ** 2 / 2 ) ) * t )  /  (sigma * np.sqrt(t))
+    d2 = d1 - sigma * np.sqrt(t)
+
+    return d1, d2
+
 
 def black_scholes_stdlib(s: float, k: float, r: float, sigma: float, t: float, flag: str, d1: float = None, d2: float = None) -> float:
     """
@@ -27,8 +72,7 @@ def black_scholes_stdlib(s: float, k: float, r: float, sigma: float, t: float, f
         raise ValueError("Invalid option type")   
 	
 
-
-def black_scholes_stdlib(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.arra, flag: str, d1: np.array = None, d2: np.array = None) -> np.array:
+def black_scholes_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, flag: str, d1: np.array = None, d2: np.array = None) -> np.array:
     """
     Calculates the delta of a european option (call or put), whose underlying pays no dividends, using numpy & scipy for speed & vectorized operations
 
@@ -47,15 +91,15 @@ def black_scholes_stdlib(s: np.array, k: np.array, r: np.array, sigma: np.array,
     """
 
     if flag.lower() == "c":
-        return	bs_call_vector(s, k, r, sigma, t, q, d1, d2)
+        return	bs_call_vector(s, k, r, sigma, t, d1, d2)
     elif flag.lower() == "p":
-        return	bs_put_vector(s, k, r, sigma, t, q, d1, d2)
+        return	bs_put_vector(s, k, r, sigma, t, d1, d2)
     else:
         raise ValueError("Invalid option type")
 
 
 
-def bs_call_stdlib(s: float, k: float, r: float, sigma: float, t: float) -> float:
+def bs_call_stdlib(s: float, k: float, r: float, sigma: float, t: float, d1: float = None, d2: float = None) -> float:
     """
     Calculates the price of a european call option whose underlying pays no dividends, using the Python standard library
 
@@ -65,13 +109,15 @@ def bs_call_stdlib(s: float, k: float, r: float, sigma: float, t: float) -> floa
         r (_float_): Risk-free rate (Annual)
         sigma (_float_): Volatility of the underlying (Annual)
         t (_float_): Time to expiration
+        d1 (_float_): The precalculated value for d1
+        d2 (_float_): The precalculated value for d2
 
     Returns:
         float: Price of the call option
     """
 
-	if d1 == None:
-        d1 = ( math.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * math.sqrt(t))
+    if d1 == None:
+        d1 = ( math.log( s/k ) + (r + ( sigma ** 2 / 2 )) * t ) / (sigma * math.sqrt(t))
 
     if d2 == None:
         d2 = d1 - sigma * math.sqrt(t)
@@ -81,7 +127,7 @@ def bs_call_stdlib(s: float, k: float, r: float, sigma: float, t: float) -> floa
     return call_price
 
 
-def bs_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array) -> np.array:
+def bs_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, d1: np.array = None, d2: np.array = None) -> np.array:
     """
     Calculates the price of a european call option whose underlying pays no dividends, using numpy & scipy for speed & vectorized operations
 
@@ -91,13 +137,15 @@ def bs_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np
         r (_np.array_): Risk-free rate (Annual)
         sigma (_np.array_): Volatility of the underlying (Annual)
         t (_np.array_): Time to expiration
+        d1 (_np.array_): The precalculated value for d1
+        d2 (_np.array_): The precalculated value for d2
 
     Returns:
         np.array: Price of the call option
     """
 	
     if d1 == None:
-        d1 = ( np.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * np.sqrt(t))
+        d1 = ( math.log( s/k ) + (r + ( sigma ** 2 / 2 )) * t ) / (sigma * math.sqrt(t))
 
     if d2 == None:
         d2 = d1 - sigma * np.sqrt(t)
@@ -107,7 +155,7 @@ def bs_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np
     return call_price
 
 
-def bs_put_stdlib(s: float, k: float, r: float, sigma: float, t: float) -> float:
+def bs_put_stdlib(s: float, k: float, r: float, sigma: float, t: float, d1: float = None, d2: float = None) -> float:
     """
     Calculates the price of a european put option whose underlying pays no dividends, using the Python standard library
 
@@ -117,13 +165,15 @@ def bs_put_stdlib(s: float, k: float, r: float, sigma: float, t: float) -> float
         r (_float_): Risk-free rate (Annual)
         sigma (_float_): Volatility of the underlying (Annual)
         t (_float_): Time to expiration
+        d1 (_float_): The precalculated value for d1
+        d2 (_float_): The precalculated value for d2
 
     Returns:
         float: Price of the put option
     """
 
-	if d1 == None:
-        d1 = ( math.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * math.sqrt(t))
+    if d1 == None:
+        d1 = ( math.log( s/k ) + (r + ( sigma ** 2 / 2 )) * t ) / (sigma * math.sqrt(t))
 
     if d2 == None:
         d2 = d1 - sigma * math.sqrt(t)
@@ -142,13 +192,15 @@ def bs_put_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.
         r (_np.array_): Risk-free rate (Annual)
         sigma (_np.array_): Volatility of the underlying (Annual)
         t (_np.array_): Time to expiration
+        d1 (_np.array_): The precalculated value for d1
+        d2 (_np.array_): The precalculated value for d2    
 
     Returns:
         np.array: Price of the put option
     """
 	
     if d1 == None:
-        d1 = ( np.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * np.sqrt(t))
+        d1 = ( math.log( s/k ) + (r + ( sigma ** 2 / 2 )) * t ) / (sigma * math.sqrt(t))
 
     if d2 == None:
         d2 = d1 - sigma * np.sqrt(t)    
