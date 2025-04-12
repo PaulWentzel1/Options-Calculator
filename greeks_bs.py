e2f49bb7322de49e55340492126ef61ab1fb354f
Paul Wentzel <paul19wen@gmail.com>
2025-04-12 09:48:32 +0200

    Update greeks_bs.py

--- greeks_bs.py
+++ greeks_bs.py
@@ -4,7 +4,10 @@ from statistics import NormalDist
 import numpy as np
 from scipy.stats import norm
 
-# Functions for Greeks on european options paying a Continuous dividend yield (Black-Scholes-Merton)
+# Functions for Greeks on european options paying a no dividend (Black-Scholes)
+
+# d1 = ( math.log( s/k ) + ( r + ( sigma ** 2 / 2 ) ) * t )  /  (sigma * math.sqrt(t))
+# d2 = d1 - sigma * math.sqrt(t)
 
 def delta_call_stdlib(s: float, k: float, r: float, sigma: float, t: float, d1: float = None) -> float:
     """
@@ -23,16 +26,16 @@ def delta_call_stdlib(s: float, k: float, r: float, sigma: float, t: float, d1: 
     """
 
     if d1 == None:
-        d1 = ( math.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * math.sqrt(t))
+        d1 = (math.log(s/k) + (r + (sigma ** 2 / 2)) * t) / (sigma * math.sqrt(t))
 
-    delta = math.exp(-q*t) * NormalDist().cdf(d1)
+    delta = NormalDist().cdf(d1)
 
     return delta
 
 
-def delta_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, d1: np.array = None) -> np.array:
+def delta_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, d1: np.array = None) -> np.array:
     """
-    Calculates the delta of a european call option, whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
+    Calculates the delta of a european call option, whose underlying pays no dividend, using numpy & scipy for speed & vectorized operations
 
     Args:
         s (_np.array_): Current price of the underlying
@@ -40,23 +43,22 @@ def delta_call_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t:
         r (_np.array_): Risk-free rate (Annual)
         sigma (_np.array_): Volatility of the underlying (Annual)
         t (_np.array_): Time to expiration
-        q (_np.array_): Continuous dividend yield
         d1 (_np.array_): The precalculated value for d1
     Returns:
         np.array: The delta of the option
     """
 
     if d1 == None:
-        d1 = ( np.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t )  /  (sigma * np.sqrt(t))
+        d1 = (np.log(s / k) + (r  + (sigma ** 2 / 2)) * t) / (sigma * np.sqrt(t))
     
-    delta = np.exp(-q*t) * norm.cdf(d1)
+    delta = norm.cdf(d1)
 
     return delta
 
 
-def delta_put_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, d1: float = None) -> float:
+def delta_put_stdlib(s: float, k: float, r: float, sigma: float, t: float, d1: float = None) -> float:
     """
-    Calculates the delta of a european put option, whose underlying pays a continuous dividend yield q, using the Python standard library
+    Calculates the delta of a european put option, whose underlying pays no dividend, using the Python standard library
 
     Args:
         s (_float_): Current price of the underlying
@@ -64,7 +66,6 @@ def delta_put_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: fl
         r (_float_): Risk-free rate (Annual)
         sigma (_float_): Volatility of the underlying (Annual)
         t (_float_): Time to expiration
-        q (_float_): Continuous dividend yield
         d1 (_float_): The precalculated value for d1
 
     Returns:
@@ -72,16 +73,16 @@ def delta_put_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: fl
     """
 
     if d1 == None:
-        d1 = ( math.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * math.sqrt(t))
+        d1 = (math.log(s/k) + (r + (sigma ** 2 / 2)) * t) / (sigma * math.sqrt(t))
     
-    delta = -math.exp(-q*t) * NormalDist().cdf(-d1)
+    delta = NormalDist().cdf(d1) - 1
 
     return delta
 
 
-def delta_put_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, d1: np.array = None) -> np.array:
+def delta_put_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, d1: np.array = None) -> np.array:
     """
-    Calculates the delta of a european put option, whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
+    Calculates the delta of a european put option, whose underlying pays no dividend, using numpy & scipy for speed & vectorized operations
 
     Args:
         s (_np.array_): Current price of the underlying
@@ -89,7 +90,6 @@ def delta_put_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: 
         r (_np.array_): Risk-free rate (Annual)
         sigma (_np.array_): Volatility of the underlying (Annual)
         t (_np.array_): Time to expiration
-        q (_np.array_): Continuous dividend yield
         d1 (_np.array_): The precalculated value for d1
 
     Returns:
@@ -97,16 +97,16 @@ def delta_put_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: 
     """
 
     if d1 == None:
-        d1 = ( np.log( s/k ) + ( r - q + ( sigma ** 2 / 2 ) ) * t ) / (sigma * np.sqrt(t))
+        d1 = (np.log(s / k) + (r + (sigma ** 2 / 2)) * t) / (sigma * np.sqrt(t))
 
-    delta = -np.exp(-q*t) * norm.cdf(-d1)
+    delta = norm.cdf(d1) - 1
 
     return delta
 
 
-def delta_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, flag: str, d1: float = None):
+def delta_stdlib(s: float, k: float, r: float, sigma: float, t: float, flag: str, d1: float = None):
     """
-    Calculates the delta of a european option (call or put), whose underlying pays a continuous dividend yield q, using the Python standard library
+    Calculates the delta of a european option (call or put), whose underlying pays no dividend, using the Python standard library
 
     Args:
         s (_float_): Current price of the underlying
@@ -114,25 +114,24 @@ def delta_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float,
         r (_float_): Risk-free rate (Annual)
         sigma (_float_): Volatility of the underlying (Annual)
         t (_float_): Time to expiration
-        q (_float_): Continuous dividend yield
         flag (_str_): Determines the type of the option
-        d1 (_float_): The precalculated value for d1
+        d1 (_float_): The recalculated value for d1
 
     Returns:
         float: The delta of the option
     """
 
     if flag.lower() == "c":
-        return delta_call_stdlib(s, k, r, sigma, t, q, d1)
+        return delta_call_stdlib(s, k, r, sigma, t, d1)
     elif flag.lower() == "p":
-        return delta_put_stdlib(s, k, r, sigma, t, q, d1)
+        return delta_put_stdlib(s, k, r, sigma, t, d1)
     else:
         raise ValueError("Invalid option type")
 
 
-def delta_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, q: np.array, flag: str, d1: np.array = None) -> np.array:
+def delta_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.array, flag: str, d1: np.array = None) -> np.array:
     """
-    Calculates the delta of a european option (call or put), whose underlying pays a continuous dividend yield q, using numpy & scipy for speed & vectorized operations
+    Calculates the delta of a european option (call or put), whose underlying pays no dividend, using numpy & scipy for speed & vectorized operations
 
     Args:
         s (_np.array_): Current price of the underlying
@@ -140,7 +139,6 @@ def delta_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.a
         r (_np.array_): Risk-free rate (Annual)
         sigma (_np.array_): Volatility of the underlying (Annual)
         t (_np.array_): Time to expiration
-        q (_np.array_): Continuous dividend yield
         flag (_str_): Determines the type of the option
         d1 (_np.array_): The precalculated value for d1
         
@@ -149,12 +147,14 @@ def delta_vector(s: np.array, k: np.array, r: np.array, sigma: np.array, t: np.a
     """
 
     if flag.lower() == "c":
-        return	delta_call_vector(s, k, r, sigma, t, q, d1)
+        return	delta_call_vector(s, k, r, sigma, t, d1)
     elif flag.lower() == "p":
-        return	delta_put_vector(s, k, r, sigma, t, q, d1)
+        return	delta_put_vector(s, k, r, sigma, t, d1)
     else:
         raise ValueError("Invalid option type")
 
+
+# Left off here
 
 def vega_stdlib(s: float, k: float, r: float, sigma: float, t: float, q: float, d1: float = None):
     """
